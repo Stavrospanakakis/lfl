b625becba7ebe693bc1e873b57c4648efaa544a5
Stavros Panakakis <stavrospanakakis@gmail.com>
2021-01-18 07:35:51 +0200

    Separate a readlectures function from services

--- internal/services/services.go
+++ internal/services/services.go
@@ -1,9 +1,6 @@
 package services
 
 import (
-	"encoding/json"
-	"os"
-
 	"github.com/Stavrospanakakis/lfl/internal/models"
 )
 
@@ -16,25 +13,4 @@ type Service struct {
 func MakeService() *Service {
 	s := &Service{}
 	return s
-}
-
-// ReadLecturesFromConfigurationFile reads lectures from configuration file
-func (s *Service) ReadLecturesFromConfigurationFile(configPath string) error {
-	file, err := os.Open(configPath)
-	defer file.Close()
-
-	if err != nil {
-		return err
-	}
-
-	var lectures []models.Lecture
-
-	decoder := json.NewDecoder(file)
-
-	err = decoder.Decode(&lectures)
-	if err != nil {
-		return err
-	}
-	s.Lectures = lectures
-	return nil
 }
