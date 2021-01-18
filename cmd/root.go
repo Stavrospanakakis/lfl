0b6c8c833d9d2a2a06ab9b182287e0df569d930c
Stavros Panakakis <stavrospanakakis@gmail.com>
2021-01-18 07:25:40 +0200

    Refactor code

--- cmd/root.go
+++ cmd/root.go
@@ -4,15 +4,9 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/Stavrospanakakis/lfl/internal/services"
 	"github.com/spf13/cobra"
-
-	homedir "github.com/mitchellh/go-homedir"
-	"github.com/spf13/viper"
-
-	"github.com/Stavrospanakakis/lfl/services"
 )
-
-var cfgFile string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -21,10 +15,22 @@ var rootCmd = &cobra.Command{
 	Long: `lfl (Links for Lectures) opens the link of the lecture of your choice
 in zoom. You add the subjects' info in your config file and you are done.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		lectureID, _ := cmd.Flags().GetString("lecture")
-		err := services.GetLinkByID(lectureID)
+		s := services.MakeService()
+		configPath, err := s.GetConfigPath()
+
 		if err != nil {
 			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		if s.ConfigurationFileExists(configPath) {
+			s.Run(configPath)
+		} else {
+			err := s.GenerateConfig(configPath)
+			if err != nil {
+				fmt.Println(err)
+			}
+			os.Exit(1)
 		}
 
 	},
@@ -32,47 +38,10 @@ in zoom. You add the subjects' info in your config file and you are done.`,
 
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
-// It also shows a list of the lectures
 func Execute() {
-	if err := rootCmd.Execute(); err != nil {
+	err := rootCmd.Execute()
+	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-	lFlag, _ := rootCmd.Flags().GetString("lecture")
-	if lFlag == "" {
-		err := services.ShowLecturesList()
-		if err != nil {
-			fmt.Println(err)
-		}
-	}
-}
-
-func init() {
-	cobra.OnInitialize(initConfig)
-	var lecture string
-
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.lfl.json)")
-	rootCmd.Flags().StringVarP(&lecture, "lecture", "l", "", "The id of the lecture of your choice (Type lfl to see the available ids)")
-}
-
-// initConfig reads in config file and ENV variables if set.
-func initConfig() {
-	if cfgFile != "" {
-		// Use config file from the flag.
-		viper.SetConfigFile(cfgFile)
-	} else {
-		// Find home directory.
-		home, err := homedir.Dir()
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-
-		// Search config in home directory with name ".lfl" (without extension).
-		viper.AddConfigPath(home)
-		viper.SetConfigType("json")
-		viper.SetConfigName(".lfl.json")
-	}
-
-	viper.AutomaticEnv() // read in environment variables that match
 }
