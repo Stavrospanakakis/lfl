2a784bd29ecc1d3128c809b68073cd967d773513
Stavros Panakakis <stavrospanakakis@gmail.com>
2021-01-18 07:26:42 +0200

    Refactor function

--- internal/services/openMeeting.go
+++ internal/services/openMeeting.go
@@ -3,7 +3,7 @@ package services
 import "os/exec"
 
 // OpenMeeting opens the browser and pastes the url
-func OpenMeeting(link string) error {
+func (s *Service) OpenMeeting(link string) error {
 	err := exec.Command("xdg-open", link).Start()
 	if err != nil {
 		return err
