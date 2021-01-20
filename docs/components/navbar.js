286f17040a62059e7a4b9cbd342afbbd68185ef3
Stavros Panakakis <stavrospanakakis@gmail.com>
2021-01-20 13:12:31 +0200

    Fix navbar navigation

--- docs/components/navbar.js
+++ docs/components/navbar.js
@@ -3,14 +3,27 @@ import { useState } from 'react'
 export default function Navbar() {
     const [isClicked, setisClicked] = useState(false)
 
-    function handleClick() {
+    const handleClick = () => {
         setisClicked(!isClicked)
     }
 
+    const scrollToInstallation = () => {
+      const section = document.getElementById('#installation');
+      section?.scrollIntoView( { behavior: 'smooth', block: 'start' } )
+      setisClicked(false)
+    }
+
+    const scrollToUsage = () => {
+      const section = document.getElementById('#usage');
+      section?.scrollIntoView( { behavior: 'smooth', block: 'start' } )
+      setisClicked(false)
+    }
+
+   
     return (
         <header className="fixed bg-black top-0 w-full overflow-hidden sm:flex sm:flex-wrap sm:items-center sm:justify-between px-2 py-3">
         <div className="container mx-auto px-4 sm:flex sm:flex-wrap sm:items-center sm:justify-between">
-          <a to="/" className="text-lg font-bold text-white">lfl - Links for Lectures</a>
+          <a href="/" className="text-lg font-bold text-white">lfl - Links for Lectures</a>
           <button onClick={handleClick} className="sm:hidden text-white float-right text-xl leading-none px-3 py-1">
              <span className="block relative w-6 rounded-sm bg-white border-white border mt-1"></span>
              <span className="block relative w-6 rounded-sm bg-white mt-1 border"></span>
@@ -18,13 +31,13 @@ export default function Navbar() {
           </button>
           {isClicked? 
           <div className="block">
-              <a href="#installation" className="text-white pt-2 rounded py-1 block sm:inline-block">Installation</a>
-              <a href="#usage" className="text-white rounded py-1 block sm:inline-block">Usage</a>
+              <div onClick={scrollToInstallation} className="text-white pt-2 rounded py-1 block sm:inline-block">Installation</div>
+              <div onClick={scrollToUsage} className="text-white rounded py-1 block sm:inline-block">Usage</div>
           </div>
           :
           <div>
-              <a href="#installation" className="text-white px-3 rounded py-1 hidden sm:inline-block">Installation</a>
-              <a href="#usage" className="text-white hover:bg-gray-700 px-3 rounded py-1 hidden sm:inline-block">Usage</a>
+              <div onClick={scrollToInstallation} className="text-white px-3 rounded py-1 hidden sm:inline-block cursor-pointer">Installation</div>
+              <div onClick={scrollToUsage} className="text-white px-3 rounded py-1 hidden sm:inline-block cursor-pointer">Usage</div>
           </div>
           }
         </div>
