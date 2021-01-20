e23f942c40fa9517a1d95173f0dac23d214fbcd1
Stavros Panakakis <stavrospanakakis@gmail.com>
2021-01-20 12:43:35 +0200

    Add humburger menu

--- docs/components/navbar.js
+++ docs/components/navbar.js
@@ -11,9 +11,11 @@ export default function Navbar() {
         <header className="fixed bg-black top-0 w-full overflow-hidden sm:flex sm:flex-wrap sm:items-center sm:justify-between px-2 py-3">
         <div className="container mx-auto px-4 sm:flex sm:flex-wrap sm:items-center sm:justify-between">
           <a to="/" className="text-lg font-bold text-white">lfl - Links for Lectures</a>
-          <button onClick={handleClick} className="sm:hidden text-white float-right">
-             menu
-           </button>
+          <button onClick={handleClick} className="sm:hidden text-white float-right text-xl leading-none px-3 py-1">
+             <span className="block relative w-6 rounded-sm bg-white border-white border mt-1"></span>
+             <span className="block relative w-6 rounded-sm bg-white mt-1 border"></span>
+             <span className="block relative w-6 rounded-sm bg-white mt-1 border"></span>
+          </button>
           {isClicked? 
           <div className="block">
               <a href="#installation" className="text-white pt-2 rounded py-1 block sm:inline-block">Installation</a>
