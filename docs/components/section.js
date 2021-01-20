f2c258de07fbbd2789f6a6a8e249b7119fe8997d
Stavros Panakakis <stavrospanakakis@gmail.com>
2021-01-20 12:34:57 +0200

    Add padding to sections

--- docs/components/section.js
+++ docs/components/section.js
@@ -1,6 +1,6 @@
 export default function Section(props) {
     return (
-        <div className="mt-28" id={props.id}>
+        <div className="px-2 py-3 mt-12" id={props.id}>
             <div className="font-bold text-2xl lg:text-4xl">{props.title}</div>
             <div className="text-lg lg:text-xl italic text-gray-600">{props.subtitle}</div>
             <div className="">{props.description}</div>
