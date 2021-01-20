cbe4f31f0711db6a9e9af01c4610bece62e7533c
Stavros Panakakis <stavrospanakakis@gmail.com>
2021-01-20 12:34:31 +0200

    Seperate footer to component

---
+++ docs/components/footer.js
@@ -0,0 +1,8 @@
+export default function Footer() {
+    return (
+        <footer>
+              <div className="bg-black border border-gray-300 mt-10"></div>
+              <div align="center" className="mt-2 p-5">© 2021 Stavros Panakakis</div>
+        </footer>
+    )
+}
